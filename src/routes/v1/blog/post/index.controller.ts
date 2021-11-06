ee2fe6ed306c357b67933125d3d5852037a7794f
HADMARINE <contact@hadmarine.com>
2021-11-06 09:00:07 +0900

    Update routers

--- src/routes/v1/blog/post/index.controller.ts
+++ src/routes/v1/blog/post/index.controller.ts
@@ -1,12 +1,19 @@
+import BlogPost, { BlogPostInterface } from '@models/BlogPost';
 import packageSettings from '@src/../package.json';
 import {
   Controller,
   GetMapping,
   SetSuccessMessage,
+  WrappedRequest,
 } from 'express-quick-builder';
 
 @Controller
 export default class BlogPostController {
+  @GetMapping('/all')
+  async getAll(): Promise<BlogPostInterface[]> {
+    return await BlogPost.find().sort('-date');
+  }
+
   @GetMapping()
-  index(): Record<string, any>[] {}
+  async getWithPagination(req: WrappedRequest): Promise<BlogPostInterface[]> {}
 }
