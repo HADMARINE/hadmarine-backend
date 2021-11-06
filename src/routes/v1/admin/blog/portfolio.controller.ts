d8d0fcfb0a032e755e61706948563d323e016be2
HADMARINE <contact@hadmarine.com>
2021-11-06 13:15:02 +0900

    Update routers

--- src/routes/v1/admin/blog/portfolio.controller.ts
+++ src/routes/v1/admin/blog/portfolio.controller.ts
@@ -1,9 +1,12 @@
 import Portfolio from '@models/Portfolio';
 import packageSettings from '@src/../package.json';
+import { QueryBuilder } from '@util/Assets';
 import { AdminAuthority } from '@util/Middleware';
 import {
   Controller,
   DataTypes,
+  DeleteMapping,
+  PatchMapping,
   PostMapping,
   SetMiddleware,
   WrappedRequest,
@@ -22,7 +25,7 @@ export default class AdminPortfolioController {
       link: DataTypes.objectNull(),
     });
 
-    const portfolio = await Portfolio.create({
+    await Portfolio.create({
       title,
       subtitle,
       type,
@@ -30,5 +33,39 @@ export default class AdminPortfolioController {
       link,
       date: Date.now(),
     });
+  }
+
+  @PatchMapping(':id')
+  @SetMiddleware(AdminAuthority)
+  async modify(req: WrappedRequest): Promise<null | void> {
+    const { id } = req.verify.params({ id: DataTypes.string() });
+    const { title, subtitle, type, content, link } = req.verify.body({
+      title: DataTypes.stringNull(),
+      subtitle: DataTypes.stringNull(),
+      type: DataTypes.stringNull(),
+      content: DataTypes.stringNull(),
+      link: DataTypes.objectNull(),
+    });
+
+    await Portfolio.findByIdAndUpdate(id, {
+      $set: QueryBuilder({
+        title,
+        subtitle,
+        type,
+        content,
+        link,
+      }),
+    });
+  }
+
+  @DeleteMapping(':id')
+  @SetMiddleware(AdminAuthority)
+  async delete(req: WrappedRequest): Promise<null | void> {
+    const { id } = req.verify.params({
+      id: DataTypes.string(),
+    });
+
+    const portfolio = await Portfolio.findByIdAndDelete(id);
+    return portfolio ? undefined : null;
   }
 }
